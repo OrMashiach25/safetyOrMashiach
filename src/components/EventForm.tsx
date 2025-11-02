ab63338d723591502749de7dde60e8d35315670b
Or Mashiach <ormashi12@gmail.com>
2025-11-02 14:57:05 +0200

    יצירת קומפוננטת אב בעבור איגוד השדות

---
+++ src/components/EventForm.tsx
@@ -0,0 +1,23 @@
+
+import activityType from "./ActivityType";
+import category from "./Category";
+import descriptionEvent from "./DescriptionEvent";
+import eventSeverity from "./EventSeverity";
+import location from "./Location";
+import results from "./Results";
+import subSubUnitInput from "./SubUnitInput";
+import timeAndDate from "./TimeAndDate";
+import weather from "./Weather";
+
+
+
+activityType
+category
+descriptionEvent
+eventSeverity
+location
+results
+subSubUnitInput
+timeAndDate
+weather
+
