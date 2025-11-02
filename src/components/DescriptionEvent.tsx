753fe87a00b2c4ad3c9852cfa4d253abef30803d
Or Mashiach <ormashi12@gmail.com>
2025-11-02 09:28:56 +0200

    הוספת שימוש ב- regex לחלק של תיור האירוע

--- src/components/DescriptionEvent.tsx
+++ src/components/DescriptionEvent.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 
 function descriptionEvent () {
+    const [description, setDescription] = useState("");
+    function handleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+        const value = e.target.value;
+        const regex = /^[א-תa-zA-Z0-9\s.,!?'"()-]*$/;
+        if (regex.test(value) && value.length <= 800) {
+        setDescription(value);
+    }
+}
+
     return(
         <label>
             תיאור האירוע:
-            <textarea rows={5} placeholder="עד 800 תווים"></textarea>
+            <textarea rows={3} placeholder="עד 800 תווים" value={description} onChange={handleChange}>
+            </textarea>
         </label>
 
     );
