d486405cb3275a53fe2b13054c0af526220bbc0d
Or Mashiach <ormashi12@gmail.com>
2025-11-02 14:08:08 +0200

    הוספת תנאי לשעה שאי אפשר לבחור בשעה ותאריך מעבר לתאריך והשעה הנוכחיים

--- src/components/DescriptionEvent.tsx
+++ src/components/DescriptionEvent.tsx
@@ -7,17 +7,14 @@ function descriptionEvent () {
         const regex = /^[א-תa-zA-Z0-9\s.,!?'"()-]*$/;
         if (regex.test(value) && value.length <= 800) {
         setDescription(value);
+        }
     }
-}
-
     return(
         <label>
             תיאור האירוע:
             <textarea rows={3} placeholder="עד 800 תווים" value={description} onChange={handleChange}>
             </textarea>
         </label>
-
     );
 }
-
 export default descriptionEvent;
--- src/components/TimeAndDate.tsx
+++ src/components/TimeAndDate.tsx
@@ -1,12 +1,12 @@
 
 function timeAndDate() {
     return(
-          <label>
-            תאריך ושעה:
-            <input type="datetime-local" />
-          </label>
-
+      <label>
+        תאריך ושעה:
+        <input type="datetime-local" 
+        max={new Date().toISOString().slice(0,16)}
+        />
+      </label>
     );
 }
-
 export default timeAndDate;
