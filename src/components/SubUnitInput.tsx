f799be18e43583ff9b58f86634ee941fac0f7150
Or Mashiach <ormashi12@gmail.com>
2025-11-02 13:38:41 +0200

    הוספת הערת שגיאה להזנה לא חוקית

--- src/components/SubUnitInput.tsx
+++ src/components/SubUnitInput.tsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
+
 
 function subSubUnitInput () {
+    const [unit, setUnit ]= useState("");
+    const [error, setError] = useState("");
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        const value= e.target.value;
+        const regex = /^[א-תa-zA-Z\s-]*$/;
+
+        if (regex.test(value)){
+            setUnit(value);
+            setError("");
+        } else {
+            setError ("ערך לא תקין");
+        }
+    }
+
+
     return(
         <label>
             יחידות משנה:
-            <input type="text" placeholder="טקסט חופשי" />
+            <input 
+            type="text" 
+            placeholder="טקסט חופשי"
+            value={unit}
+            onChange={handleChange}
+            />
+            {error && <p style={{color:"red"}}>{error}</p>}
         </label>
-
     );
 }
 
