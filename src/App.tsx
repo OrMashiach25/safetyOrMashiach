fd8f2d4a84bfc3f8ff287a57ee61a2712fb6dcac
Or Mashiach <ormashi12@gmail.com>
2025-11-03 16:48:36 +0200

    העברת הקומפוננטות של ה- Navbar ל- folder אחר לסידור נקי יותר ויכולת מעקב

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css"; 
-import Navbar from "./components/Navber";
+import Navbar from "./components/NavbarComponents/Navber";
 import EventForm from "./components/EventForm";
 
 function App() {
